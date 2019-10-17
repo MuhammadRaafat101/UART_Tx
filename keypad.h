cb6f1a6d05aa39f9f9f2e7a5401630e9fe9d0725
Mraafat32 <muhammadraafatrashad@gmail.com>
2019-10-17 16:20:13 +0200

    Create keypad.h

---
+++ keypad.h
@@ -0,0 +1,43 @@
+ /******************************************************************************
+ *
+ * Module: KEYPAD
+ *
+ * File Name: keypad.h
+ *
+ * Description: Header file for the Keypad driver
+ *
+ * Author: Mohamed Tarek
+ *
+ *******************************************************************************/
+
+#ifndef KEYPAD_H_
+#define KEYPAD_H_
+
+
+#include "UNIT_types.h"
+#include "INTERFACE.h"
+#include <avr/io.h>
+
+/*******************************************************************************
+ *                      Preprocessor Macros                                    *
+ *******************************************************************************/
+
+/* Keypad configurations for number of rows and columns */
+#define N_col 3
+#define N_row 3
+
+/* Keypad Port Configurations */
+#define KEYPAD_PORT_OUT PORTC
+#define KEYPAD_PORT_IN  PINC
+#define KEYPAD_PORT_DIR DDRC
+
+/*******************************************************************************
+ *                      Functions Prototypes                                   *
+ *******************************************************************************/
+
+/*
+ * Function responsible for getting the pressed keypad key
+ */
+unit8 KeyPad_getPressedKey(void);
+
+#endif /* KEYPAD_H_ */
