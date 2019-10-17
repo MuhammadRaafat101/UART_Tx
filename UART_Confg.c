d2289bafcc269407199e390dc155367851839f7f
Mraafat32 <muhammadraafatrashad@gmail.com>
2019-10-17 16:21:00 +0200

    Create UART_Confg.c

---
+++ UART_Confg.c
@@ -0,0 +1,14 @@
+/*
+ * UART_Confg.c
+ *
+ *  Created on: Oct 11, 2019
+ *      Author: Rafaat
+ */
+
+#include "UART_Confg.h"
+
+
+UART_cnfg_t UART_cnfg  =
+{
+		TX_EN,RX_EN,UBRR_LOAD,NA,NA,DISABLED,ONE_STOP_BIT,_8_BITS,RISING_EDGE
+};
