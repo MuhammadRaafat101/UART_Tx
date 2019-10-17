8b4d47bf60bc17e7d9ca1b310bd0c37b52379684
Mraafat32 <muhammadraafatrashad@gmail.com>
2019-10-17 16:20:00 +0200

    Create DIO_cnfg.c

---
+++ DIO_cnfg.c
@@ -0,0 +1,13 @@
+/*
+ * DIO_cnfg.c
+ *
+ * Created on: Sep 20, 2019
+ * 		Author: Mina Raouf
+ */ 
+#include "DIO_cnfg.h"
+
+DIO_cnfg_t DIO_cnfg_arr [NUM_OF_PINS] =
+{
+	{PORT_C,PIN4,INPUT,NA,NA,INITIALIZED},
+	{PORT_B,PIN2,INPUT,NA,NA,INITIALIZED}
+};
